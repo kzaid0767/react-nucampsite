efda64cb7e42df9650e4ba795e37d99b31aa4812
Kassim Zaid <kzaid0767@gmail.com>
2022-04-13 16:05:50 -0400

    react redux form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent'
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import {connect} from 'react-redux';
 
