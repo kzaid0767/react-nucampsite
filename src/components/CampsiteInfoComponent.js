b48416016508e883dd3e3f329c9aef85b178534d
Kassim Zaid <kzaid0767@gmail.com>
2022-04-23 15:32:19 -0400

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -3,6 +3,7 @@ import React, { Component }  from 'react';
 import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label, Col, Row} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import {Control, LocalForm, Errors} from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 /*import { PARTNERS } from '../shared/partners';
 import { COMMENTS } from '../shared/comments';*/
 
@@ -144,6 +145,26 @@ function RenderCampsite({campsite}) {
 
 
 function CampsiteInfo (props) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
     if (props.campsite) {
         return (
             <div className="container">
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from './ActionTypes'; //* wild imports all named exports from ActionTypes
+import { CAMPSITES } from '../shared/campsites';
 
 export const addComment = (campsiteId, rating, author, text) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -8,4 +9,27 @@ export const addComment = (campsiteId, rating, author, text) => ({
         author: author,
         text: text
     }
+});
+
+export const fetchCampsites = () => dispatch => {
+
+    dispatch(campsitesLoading());
+
+    setTimeout(() => {
+        dispatch(addCampsites(CAMPSITES));
+    }, 2000);
+}; //using time to simulate 2 secs delay 2 arrows at line 14 i.e. using redux thunk
+
+export const campsitesLoading = () => ({
+    type: ActionTypes.CAMPSITES_LOADING
+});
+
+export const campsitesFailed = errMess => ({
+    type: ActionTypes.CAMPSITES_FAILED,
+    payload: errMess
+});
+
+export const addCampsites = campsites => ({
+    type: ActionTypes.ADD_CAMPSITES,
+    payload: campsites
 });
